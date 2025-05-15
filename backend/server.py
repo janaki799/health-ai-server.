50e779ea5991d28de299e2331f7999f780532921
unknown <sangatijanaki@gmail.com>
2025-05-15 13:22:58 +0530

    Block all Firebase JSON files

--- backend/server.py
+++ backend/server.py
@@ -7,7 +7,7 @@ import os
 import firebase_admin
 app = FastAPI()
 cred = credentials.ApplicationDefault()
-cred = credentials.Certificate("medication-provider-firebase-adminsdk-fbsvc-ee3c9059f0.json")
+cred = credentials.Certificate("medication-provider-firebase-adminsdk-fbsvc-4c37b920e7.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 PORT = int(os.getenv("PORT", 10000))
